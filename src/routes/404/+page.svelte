0dbbbede97afad2876738220b05c6e4f955c7d81
lums <lums@finiteloops.net>
2026-07-02 20:35:08 +0200

    feat: 404 page

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let loaded = $state(false);
+
+	onMount(async () => {
+		await import('../../sp-app');
+		loaded = true;
+	});
+</script>
+
+<svelte:head>
+	<title>FL-404 | finite loops</title>
+</svelte:head>
+
+<div class="page">
+	<div class="container">
+		<section class="section">
+			<h3 class="section-title">FL-404</h3>
+			{#if loaded}
+				<sp-app></sp-app>
+			{/if}
+		</section>
+	</div>
+</div>
